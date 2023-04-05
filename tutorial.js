21c839d3ad6a841ffae826636a8d6bb5cb9a6607
pro <nitingoley42@gmail.com>
2023-04-05 15:53:15 +0530

    add pages for game development

---
+++ tutorial.js
@@ -0,0 +1,15 @@
+var wrapper = new wrapper(', wrapper',{
+    effect: 'coverflow',
+    grabCursor : true,
+    centerSlides:true,
+    slidePerView: 'auto',
+    coverflowEffect:
+    {
+        rotate :0,
+        stretch : 0,
+        depth :0, 
+        modifer : 1,
+        slideShadows :true,
+    },
+    loop: true,
+});
